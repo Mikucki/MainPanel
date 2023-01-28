33254efb8069a91daa953a1d7f088ba7408c8d2a
Mikucki <piotr.mikucki@op.pl>
2023-01-28 23:46:06 +0100

    added a lot

--- src/index.js
+++ src/index.js
@@ -1,48 +1,17 @@
 import "../src/main.css";
 import * as THREE from "three";
 import * as dat from "dat.gui";
-import { DirectionalLight, Texture } from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import salon from "../src/images/salon.jpg";
-import choinka from "../src/images/choinka.jpg";
-import kuchnia from "../src/images/kuchnia.jpg";
-import lazienka from "../src/images/lazienka.jpg";
-import salon2 from "../src/images/salon2.jpg";
-import konsola from "../src/images/konsola.jpg";
+import { DirectionalLight, Scene, Texture } from "three";
 
 const scene = new THREE.Scene();
 
 const textureLoader = new THREE.TextureLoader();
-
-const boxMaterials = [
-  new THREE.MeshBasicMaterial({ map: textureLoader.load(salon) }),
-  new THREE.MeshBasicMaterial({ map: textureLoader.load(choinka) }),
-  new THREE.MeshBasicMaterial({ map: textureLoader.load(kuchnia) }),
-  new THREE.MeshBasicMaterial({ map: textureLoader.load(lazienka) }),
-  new THREE.MeshBasicMaterial({ map: textureLoader.load(salon2) }),
-  new THREE.MeshBasicMaterial({ map: textureLoader.load(konsola) }),
-];
-
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshStandardMaterial({
-  color: "#ffffff",
+  color: "#808080",
 });
-const box = new THREE.Mesh(geometry, boxMaterials);
+const box = new THREE.Mesh(geometry, material);
 scene.add(box);
-
-function resizeCanvasToDisplaySize() {
-  const canvas = renderer.domElement;
-  const width = canvas.clientWidth;
-  const height = canvas.clientHeight;
-
-  if (canvas.width !== width || canvas.height !== height) {
-    renderer.setSize(width, height, false);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-  }
-}
-
-// Camera
 
 const sizes = {
   width: 400,
@@ -54,16 +23,8 @@ camera.position.z = 3;
 
 function animate() {
   box.rotation.y += 0.01;
-  let fixedNum = box.rotation.y.toFixed(0);
-  let fixedNum2 = box.rotation.z.toFixed(2);
-  console.log(fixedNum);
-
-  if (Number(fixedNum) % 2 === 0) {
-    box.rotation.z += 0.1;
-  } else {
-    box.rotation.y += 0.1;
-  }
-  resizeCanvasToDisplaySize();
+  box.rotation.z += 0.01;
+  box.rotation.x += 0.01;
 
   renderer.render(scene, camera);
 }
@@ -79,13 +40,13 @@ renderer.setSize(sizes.width, sizes.height);
 
 renderer.setAnimationLoop(animate);
 
-const ambientLight = new THREE.AmbientLight(0x333333);
-scene.add(ambientLight);
+const mainLight = new THREE.AmbientLight(0xffffff, 0.1);
+scene.add(mainLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.9);
+directionalLight.position.set(1, 1, 1);
 scene.add(directionalLight);
-directionalLight.position.set(3, 10, 0);
 
-renderer.setClearColor(0xffffff);
+renderer.setClearColor(0xffffff, 0);
 
 renderer.render(scene, camera);
