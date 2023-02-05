a44f91d991b9b0c01a1b7a377a620aa4cc5f66dc
Mikucki <piotr.mikucki@op.pl>
2023-02-05 15:32:13 +0100

    added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
+import * as textScript from "./textScript";
 import "../src/main.css";
 import * as cube from "./cube";
-import * as textScript from "./textScript";
--- src/textScript.js
+++ src/textScript.js
@@ -1,19 +1,28 @@
-const text = document.querySelector(".sec-text");
+let dataText = ["Dzownimy.", "Piszemy", "Sprzątamy", "Kupujemy"];
 
-const textLoad = () => {
-  setTimeout(() => {
-    text.textContent = "Sprzątamy";
-  }, 0);
-  setTimeout(() => {
-    text.textContent = "Dzwonimy";
-  }, 4000);
-  setTimeout(() => {
-    text.textContent = "Pracujemy";
-  }, 8000);
-  setTimeout(() => {
-    text.textContent = "Promujemy";
-  }, 12000);
-};
+function typeWriter(text, i, fnCallback) {
+  console.log(text);
+  if (i < text.length) {
+    document.querySelector("h1").innerHTML =
+      text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
 
-textLoad();
-setInterval(textLoad, 12000);
+    setTimeout(function () {
+      typeWriter(text, i + 1, fnCallback);
+    }, 200);
+  } else if (typeof fnCallback == "function") {
+    setTimeout(fnCallback, 2000);
+  }
+}
+function StartTextAnimation(i) {
+  if (typeof dataText[i] == "undefined") {
+    setTimeout(function () {
+      StartTextAnimation(0);
+    }, 2000);
+  }
+  if (i < dataText[i].length) {
+    typeWriter(dataText[i], 0, function () {
+      StartTextAnimation(i + 1);
+    });
+  }
+}
+StartTextAnimation(0);
